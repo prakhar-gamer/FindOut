68fbf1f660bff4ecf3a6b495deb765343e7d59d6
prakhar-gamer <prakhargupta0402@gmail.com>
2026-04-08 10:48:05 -0700

    added the HTML for the Website working on the Flask CRUD implementation

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, send_from_directory, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+ 
+#makes the database so we cna implemt CRUD into the Phrase
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class Phrase(db.Model):
+    phrase = db.Column(db.Text, nullable=False)
+    pid = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(30), nullable=False)
+
+with app.app_context():
+    db.create_all()
+    
+
 
 @app.route("/check1", methods=['POST'])
 def check1():
@@ -9,8 +24,6 @@ def check1():
         return redirect("/DudeYourAGenius")
     
     return "try again :()"
-    
-
 
 @app.route("/")
 #play a audio clip -> unscramble it and have a button gives u the link to the seocund part
@@ -24,17 +37,15 @@ def getAudio():
 @app.route("/DudeYourAGenius")
 #Allows you to make a account and update it to crude
 def Hobbies():
-    return"I like the gym, computers, and music"
+    return render_template('accountcreation.html')
 
-@app.route("/login")
-#uses a auth and then if its a account lets u enter the victory screen
-def Login():
-    return"Dogs are pretty fire though"
-
-@app.route("/victory!!!!")
-#a Screen that says VICTORY!
-def victory():
-    return "Victory!"
+@app.route("/addPhrase", methods=['POST'])
+def addTask():
+    phrase = request.form.get('phrase')
+    password = request.form.get('phrasePassword')
+    id = request.form.get('phraseId')
+    return "funny"
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True);
