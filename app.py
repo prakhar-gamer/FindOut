db1e766d7cd4ad5a2e75196803fb22967329f377
prakhar-gamer <prakhargupta0402@gmail.com>
2026-04-08 12:07:58 -0700

    Added CRUD Functionality and authorized user end point thingies!

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 class Phrase(db.Model):
     phrase = db.Column(db.Text, nullable=False)
-    pid = db.Column(db.Integer, primary_key=True)
+    pid = db.Column(db.Integer(), primary_key=True)
     password = db.Column(db.String(30), nullable=False)
 
 with app.app_context():
@@ -40,11 +40,73 @@ def Hobbies():
     return render_template('accountcreation.html')
 
 @app.route("/addPhrase", methods=['POST'])
-def addTask():
+def addPhrase():
     phrase = request.form.get('phrase')
     password = request.form.get('phrasePassword')
-    id = request.form.get('phraseId')
-    return "funny"
+    pid = request.form.get('phraseId')
+
+    check_pid = Phrase.query.get(pid)
+
+    if check_pid:
+        return "The ID already Exists, try another one!"
+
+    newPhrase = Phrase(phrase=phrase,pid=int(pid),password=password)
+    db.session.add(newPhrase)
+    db.session.commit()
+    return render_template('victory.html')
+
+@app.route("/viewPhrase", methods=['POST'])
+def viewPhrase():
+    password = request.form.get('viewPhrasePassword')
+    pid = request.form.get('viewPhraseID')
+
+    phraseLookup = Phrase.query.get(pid)
+
+    if phraseLookup == None:
+        return "No IDS matches :()"
+    
+    if phraseLookup.password == password:
+        return phraseLookup.phrase
+    else:
+        return "incorect password for id"
+
+@app.route("/updatePhrase", methods=['POST'])
+def updatePhrase():
+    phrase = request.form.get('updatePhrase')
+    password = request.form.get('updatePhrasePassword')
+    pid = request.form.get('updatePhraseId')
+
+    phraseLookup = Phrase.query.get(pid)
+
+    if phraseLookup == None:
+        return "No IDS Match"
+    
+    
+    if phraseLookup.password == password:
+        updatePhrase = Phrase(phrase=phrase,pid=int(pid),password=phraseLookup.password)
+        db.session.merge(updatePhrase)
+        db.session.commit()
+        return render_template('victory.html')
+    
+    else:
+        return "not the correct password"
+    
+@app.route("/deletePhrase", methods=['POST'])
+def deletePhrase():
+    password = request.form.get('deletePhrasePassword')
+    pid = request.form.get('deletePhraseID')
+
+    phraseLookup = Phrase.query.get(pid)
+
+    if phraseLookup == None:
+        return "No IDS matches :()"
+    
+    if phraseLookup.password == password:
+        db.session.delete(phraseLookup)
+        db.session.commit()
+        return render_template('victory.html')
+    else:
+        return "incorect password for id"
     
 
 if __name__ == "__main__":
